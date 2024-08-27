a26d8aaa4e34b997633df69a84c04f73a6543c49
Jack Koontz <jack.alex.koontz@gmail.com>
2024-08-27 11:25:53 -0500

    refinement

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify, request, render_template
-from  FlightRadar24 import FlightRadar24API
+from FlightRadar24 import FlightRadar24API
 import logging
 import traceback
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
+fr_api = FlightRadar24API()  # Move this outside of the route function
 
 @app.route('/')
 def landing():
@@ -15,31 +16,32 @@ def get_flights():
     try:
         lat = float(request.args.get('lat'))
         lon = float(request.args.get('lon'))
-        # ran = float(request.args.get('ran'))
+        radius = float(request.args.get('radius', 40000))  # Add radius parameter with default value
         
-        app.logger.debug(f"Received request for lat: {lat}, lon: {lon}")
-        fr_api = FlightRadar24API()
-        bounds = fr_api.get_bounds_by_point(lat, lon, 8000)
-        # app.logger.debug(f"Calculated bounds: {bounds}")
+        app.logger.debug(f"Received request for lat: {lat}, lon: {lon}, radius: {radius}")
         
+        bounds = fr_api.get_bounds_by_point(lat, lon, radius)
         flights = fr_api.get_flights(bounds=bounds)
-        app.logger.debug(flights)
         app.logger.debug(f"Number of flights found: {len(flights)}")
         
-        flight_data = []
-        for flight in flights:
-            flight_data.append({
-                'callsign': flight.callsign,
-                'altitude': flight.altitude,
-                'speed': flight.ground_speed
-            })
+        flight_data = [{
+            'callsign': flight.callsign,
+            'altitude': flight.altitude,
+            'speed': flight.ground_speed,
+            'latitude': flight.latitude,  # Add latitude
+            'longitude': flight.longitude  # Add longitude
+        } for flight in flights]
         
         return jsonify(flight_data)
     
+    except ValueError as ve:
+        app.logger.error(f"Invalid input: {str(ve)}")
+        return jsonify({"error": "Invalid input parameters"}), 400
     except Exception as e:
         app.logger.error(f"An error occurred: {str(e)}")
         app.logger.error(traceback.format_exc())
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
